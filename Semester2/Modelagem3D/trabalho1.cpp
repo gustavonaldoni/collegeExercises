b581d373e161ec31a552fa91ad29ae1db9e73d7a
Gustavo Azevedo Naldoni <1376544@sga.pucminas.br>
2022-09-01 10:03:26 -0300

    Add first homework

---
+++ Semester2/Modelagem3D/trabalho1.cpp
@@ -0,0 +1,165 @@
+#include<GL/freeglut.h>
+#include<iostream>
+
+GLfloat escala = 1;
+GLfloat transladarX = 1;
+GLfloat transladarY = 1;
+
+GLfloat angulo = 0;
+
+void letraN(float x, float y, int tamanho)
+{
+    glBegin(GL_LINES);
+    glVertex2f(-8, tamanho/2);
+    glVertex2f(-8, -tamanho/2);
+
+    glVertex2f(-8, tamanho/2);
+    glVertex2f(-4, -tamanho/2);
+
+    glVertex2f(-4, -tamanho/2);
+    glVertex2f(-4, tamanho/2);
+
+    glVertex2f(-2, -tamanho/2);
+    glVertex2f(0, tamanho/2);
+    glEnd();
+}
+
+void desenharNome(int tamanho)
+{
+    letraN(10, 10, 16);
+
+    glBegin(GL_LINES);
+
+
+    glVertex2f(0, tamanho/2);
+    glVertex2f(2, -tamanho/2);
+
+    glVertex2f(4, tamanho/2);
+    glVertex2f(4, -tamanho/2);
+
+    glVertex2f(4, -tamanho/2);
+    glVertex2f(8, -tamanho/2);
+
+    glVertex2f(10, tamanho/2);
+    glVertex2f(10, -tamanho/2);
+
+    glVertex2f(10, tamanho/2);
+    glVertex2f(14, 0);
+
+    glVertex2f(14, 0);
+    glVertex2f(10, -tamanho/2);
+
+    glVertex2f(16, tamanho/2);
+    glVertex2f(16, -tamanho/2);
+
+    glVertex2f(16, -tamanho/2);
+    glVertex2f(20, -tamanho/2);
+
+    glVertex2f(20, -tamanho/2);
+    glVertex2f(20, tamanho/2);
+
+    glVertex2f(20, tamanho/2);
+    glVertex2f(16, tamanho/2);
+
+    glVertex2f(22, tamanho/2);
+    glVertex2f(22, -tamanho/2);
+
+    glVertex2f(22, tamanho/2);
+    glVertex2f(26, -tamanho/2);
+
+    glVertex2f(26, -tamanho/2);
+    glVertex2f(26, tamanho/2);
+
+    glVertex2f(28, tamanho/2);
+    glVertex2f(28, -tamanho/2);
+    glEnd();
+
+    glBegin(GL_POINTS);
+    glVertex2f(28, tamanho/2 + 1);
+    glEnd();
+}
+
+void desenhaTriangulo()
+{
+    glBegin(GL_TRIANGLES);
+    glColor3f(0, 1, 0);
+    glVertex2f(-50, 0);
+
+    glColor3f(1, 0, 0);
+    glVertex2f(-50, 50);
+
+    glColor3f(0, 1, 1);
+    glVertex2f(-80, 20);
+    glEnd();
+}
+
+void desenha(void)
+{
+    glClear( GL_COLOR_BUFFER_BIT );
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-100, 100, -100, 100);
+
+    glScalef(escala, escala, 0);
+    glTranslatef(transladarX, transladarY, 0);
+    glRotatef(angulo, 0, 0, 1);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    desenhaTriangulo();
+    desenharNome(10);
+
+    glFlush();
+}
+
+void listeningKey(unsigned char tecla, GLint x, GLint y)
+{
+    if (tecla == '+')
+        escala += 0.01;
+
+    else if (tecla == '-')
+        escala -= 0.01;
+
+    else if (tecla == 'a' || tecla == 'A')
+        transladarX -= 1;
+
+    else if (tecla == 'd' || tecla == 'D')
+        transladarX += 1;
+
+    else if (tecla == 'w' || tecla == 'W')
+        transladarY += 1;
+
+    else if (tecla == 's' || tecla == 'S')
+        transladarY -= 1;
+
+    else if (tecla == 'Q' || tecla == 'q')
+        angulo -= 1;
+
+    else if (tecla == 'E' || tecla == 'e')
+        angulo += 1;
+
+    if (escala >= 1.3)
+        escala = 1.3;
+
+    if (escala <= 0.5)
+        escala = 0.5;
+
+    desenha();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );
+    glutInitWindowSize(800,600);
+    glutCreateWindow("TRABALHO - GUSTAVO AZEVEDO NALDONI");
+
+    glutDisplayFunc(desenha);
+    glutKeyboardFunc(listeningKey);
+
+    glClearColor(0, 0, 0, 2);
+    glutMainLoop();
+    return 0;
+}
