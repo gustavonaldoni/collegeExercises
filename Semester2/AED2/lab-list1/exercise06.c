e482820a5af9b6272f569a22bf1e455148efc6ed
Gustavo <gunaldoni@gmail.com>
2022-09-21 19:53:54 -0300

    Add ex 6

---
+++ Semester2/AED2/lab-list1/exercise06.c
@@ -0,0 +1,28 @@
+/*
+
+Written by: Gustavo Azevedo Naldoni e André Luís Zappa
+09/21/2022
+
+QUESTION:
+
+Se i e j são variáveis inteiras e p e q ponteiros para o tipo int,
+quais das seguintes expressões de atribuição são ilegais?
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    int *p, *q;
+    
+    p = i; // Legal
+    q = &j; // Legal
+    p = &*&i; // Legal
+    i = (*&)j; // Ilegal
+    i = *&j; // Legal
+    i = *&*&j; // Legal
+    q = *p; // Legal
+    i = (*p)++ + *q; // Ilegal 
+}
