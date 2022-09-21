cee694a51dc7ffda54c824597ddaa6d3271ab7e1
Gustavo <gunaldoni@gmail.com>
2022-09-21 19:23:52 -0300

    Add ex 2

---
+++ Semester2/AED2/lab-list1/exercise02.c
@@ -0,0 +1,26 @@
+/*
+
+Written by: Gustavo Azevedo Naldoni e André Luís Zappa
+09/21/2022
+
+QUESTION:
+
+Escreva um programa na linguagem C que contenha duas variáveis inteiras. 
+Compare seus endereços e exiba o maior endereço.
+
+*/
+
+#include <stdio.h>
+
+main()
+{
+    int x, y;
+
+    //printf("%d e %d", &x, &y);
+
+    if (&x >= &y)
+        printf("%p", &x);
+
+    else if (&x < &y)
+        printf("%p", &y);
+}
